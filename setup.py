c4426c00d5ea5f3125db591eb39f8bb54e82cfd6
smaartaghu <smaartaghu@gmail.com>
2021-12-02 16:11:58 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "src",
+	version="0.0.1",
+	description="Its a wine Q Package",
+	author="Raghav",
+	packages=find_packages(),
+	license="MIT"
+)
